6ef1f7562edebbf6485610a99690fd86b80d7e1e
wprjavanguser <wprjavanguser@host.docker.internal>
2026-06-05 16:55:55 +0530

    step

--- New_Test/src/test/java/StepDefinitions_pkg/StepDefinitions.java
+++ New_Test/src/test/java/StepDefinitions_pkg/StepDefinitions.java
@@ -1,11 +1,15 @@
 package StepDefinitions_pkg;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -13,6 +17,7 @@ import io.cucumber.java.en.*;
 
 public class StepDefinitions {
 	WebDriver driver;
+	WebDriverWait wait;
 	
 	@Before
 	public void setup() {
@@ -35,6 +40,7 @@ public class StepDefinitions {
 		options.addArguments("--no-sandbox");
 
 		driver = new ChromeDriver(options);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	    driver.manage().window().maximize();
 	    driver.get("http://zero.webappsecurity.com");
 	}
@@ -52,18 +58,22 @@ public class StepDefinitions {
 
 	@Given("user click on login")
 	public void user_click_on_login() {
-	    driver.findElement(By.id("signin_button")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signin_button"))).click();
+//	    driver.findElement(By.id("signin_button")).click();
 	}
 
 	@When("^user enters (.*) and (.*)$")
 	public void user_enters_abcd_and_zxcvbnm(String user, String pass) {
-	    driver.findElement(By.id("user_login")).sendKeys(user);
-	    driver.findElement(By.id("user_password")).sendKeys(pass);
+//	    driver.findElement(By.id("user_login")).sendKeys(user);
+//	    driver.findElement(By.id("user_password")).sendKeys(pass);
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user_login"))).click();
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user_password"))).click();
 	}
 
 	@And("user click login button")
 	public void user_click_login_button() {
-	    driver.findElement(By.name("submit")).click();
+//	    driver.findElement(By.name("submit")).click();
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("submit"))).click();
 	}
 
 	@Then("validate login result")
