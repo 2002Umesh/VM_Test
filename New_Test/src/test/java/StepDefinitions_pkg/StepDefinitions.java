997291002ba506a80160ac28395bfb2929f8964a
wprjavanguser <wprjavanguser@host.docker.internal>
2026-06-05 17:59:08 +0530

    new edge

--- New_Test/src/test/java/StepDefinitions_pkg/StepDefinitions.java
+++ New_Test/src/test/java/StepDefinitions_pkg/StepDefinitions.java
@@ -23,14 +23,14 @@ public class StepDefinitions {
 	public void setup() {
 	    EdgeOptions options = new EdgeOptions();
 
-	    options.addArguments("--headless=new");
+	    options.addArguments("--headless");
 	    options.addArguments("--disable-gpu");
 	    options.addArguments("--window-size=1920,1080");
-	    options.addArguments("--no-sandbox");
-	    options.addArguments("--disable-dev-shm-usage");
-	    options.addArguments("--remote-allow-origins=*");
+	    options.addArguments("--remote-debugging-port=9222");
 
 	    driver = new EdgeDriver(options);
+
+	    driver.manage().window().maximize();
 
 //		ChromeOptions options = new ChromeOptions();
 //
