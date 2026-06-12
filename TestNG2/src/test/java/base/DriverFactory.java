576221cad47209afa541160175242d5d2d8ce84a
Administrator <Administrator@172.19.21.85>
2026-06-12 19:07:29 +0530

    final

--- TestNG2/src/test/java/base/DriverFactory.java
+++ TestNG2/src/test/java/base/DriverFactory.java
@@ -3,6 +3,7 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -14,7 +15,15 @@ public class DriverFactory {
 		} else if(browser.equalsIgnoreCase("firefox")) {
 			driver.set(new FirefoxDriver());
 		} else if(browser.equalsIgnoreCase("edge")) {
-			driver.set(new EdgeDriver());
+			EdgeOptions options = new EdgeOptions();
+
+			options.addArguments("--headless=new");
+			options.addArguments("--disable-gpu");
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--remote-allow-origins=*");
+
+			driver.set(new EdgeDriver(options));
 		} else {
 			throw new IllegalArgumentException("Browser not supported: " + browser);
 		}
