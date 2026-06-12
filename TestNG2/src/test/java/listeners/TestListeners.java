ad954e704a16fd557c6300c8050a3ec52c7efeab
Administrator <Administrator@172.19.21.85>
2026-06-12 17:42:13 +0530

    extent

--- TestNG2/src/test/java/listeners/TestListeners.java
+++ TestNG2/src/test/java/listeners/TestListeners.java
@@ -1,29 +1,58 @@
 package listeners;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
 public class TestListeners implements ITestListener {
-	private static ExtentManager extentManager = new ExtentManager();
-//	private ExtentTest test;
+	private static ExtentReports extent = ExtentManager.getReport();
+
+	private ExtentTest test;
+
 	@Override
 	public void onTestStart(ITestResult result) {
-		System.out.println("Test started: " + result.getName());
+
+		System.out.println("Started : " + result.getParameters()[1].toString());
+
+		test = extent.createTest(result.getParameters()[1].toString());
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("Test passed: " + result.getName());
+
+		System.out.println("PASS Listener Triggered");
+
+		test.pass("Test Passed");
+
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		System.out.println("Test failed: " + result.getName());
+
+		System.out.println("FAIL Listener Triggered");
+
+		test.fail(result.getThrowable());
+
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		System.out.println("Test skipped: " + result.getName());
+
+		System.out.println("SKIP Listener Triggered");
+
+		test.skip("Test Skipped");
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+
+		extent.flush();
+
+		System.out.println("Extent Report Generated Successfully");
 	}
 
 }
